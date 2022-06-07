de5a499fbbd7a9ad19bfe22d911d5c09da8623a8
KJ <10dkavyajain@gmail.com>
2022-06-07 19:06:57 +0530

    Update code

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-uri = os.envion.get("DATABASE_URL")  # or other relevant config var
+uri = os.environ.get("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
