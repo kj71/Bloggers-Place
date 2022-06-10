a70507c9beab142d009b6c74b2c9da35f1e640b8
KJ <10dkavyajain@gmail.com>
2022-06-10 20:28:34 +0530

    Added title for Login and Register Page

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ class Comment(db.Model):
     post = relationship("BlogPost", back_populates="comments")
 
 
-db.create_all()
+# db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
